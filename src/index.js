ea6a52a95bf75022bf92fab413128244a532d3ba
paulwooolf <paulwooolf@gmail.com>
2021-03-13 15:34:58 +0300

    refactor: new version

--- src/index.js
+++ src/index.js
@@ -1,35 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
-    bracketsConfig.forEach((item, index, arr) => {
-        if (item[0] === '|') {
-            arr[index] = ['<', '>'];
-        }
-    })
-    let result = true;
-    const brackets = Object.fromEntries(bracketsConfig);
     const stack = [];
-    const string = [];
-    let char = '<';
-    str.split('').reverse().map((item, index, arr) => {
-        if (item === '|') {
-            char = char === '<' ? '>' : '<'
-            string.push(char);
-
-        } else if (item === '|') {
-            char = char === '<' ? '>' : '<'
-            string.push(char);
-        } else {
-            string.push(item);
-        }
-    });
-    string.forEach((item, index) => {
-        if (item in brackets) {
-            let last = stack.pop();
-            if (brackets[item] !== last) {
-                result = false;
-            }
+    const brackets = Object.fromEntries(bracketsConfig);
+    str.split('').forEach(item => {
+        if (item === brackets[stack[stack.length-1]]) {
+            stack.pop();
         } else {
             stack.push(item);
         }
-    })
-    return result;
+    });
+    return stack.length === 0;
 }
