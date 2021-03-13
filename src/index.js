7ba03ef3970eb9a7eaabfcf5ba74989eaddd1b54
paulwooolf <paulwooolf@gmail.com>
2021-03-13 13:09:53 +0300

    feat: write brackets func

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    bracketsConfig.forEach((item, index, arr) => {
+        if (item[0] === '|') {
+            arr[index] = ['<', '>'];
+        }
+    })
+    let result = true;
+    const brackets = Object.fromEntries(bracketsConfig);
+    const stack = [];
+
+    str.split('').reverse().forEach((item, index) => {
+        if (item in brackets) {
+            if (brackets[item] !== stack.pop()) {
+                result = false;
+            }
+        } else {
+            stack.push(item);
+        }
+    })
+    return result;
 }
